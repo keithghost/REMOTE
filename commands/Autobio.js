7fb401cb8cbf8b1bf950c1f5464eed4bd3095779
keithghost <keithalphamd@gmail.com>
2025-04-19 09:44:09 +0300

    Update Autobio.js

--- commands/Autobio.js
+++ commands/Autobio.js
@@ -39,89 +39,47 @@ keith({
     }, { quoted: ms });
   }
 });
-/*keith({
-  nomCom: "vcard",
-  desc: "to generate owner vcard number",
-  categorie: "owner",
-  reaction: "⚔️"
-}, async (dest, zk, commandeOptions) => {
-  const { ms, auteurMsgRepondu, arg, nomAuteurMessage, auteurMessage } = commandeOptions;
-
-  let targetNumber, targetName;
-
-  if (auteurMsgRepondu) {
-    // If user is mentioned/replied to
-    targetNumber = auteurMsgRepondu.split('@')[0];
-    targetName = nomAuteurMessage || "User";
-  } else if (arg && arg.length > 0) {
-    // If no mention but has argument
-    const possibleNumber = arg[0].replace(/[^0-9]/g, '');
-    if (possibleNumber.length > 5) {
-      targetNumber = possibleNumber;
-      targetName = arg.join(' ').replace(possibleNumber, '').trim() || "User";
-    } else {
-      return repondre('❌ Please provide a valid phone number or mention a user');
-    }
-  } else {
-    return repondre('❌ Please mention a user or provide a phone number and name');
-  }
-
-  const vcard =
-    'BEGIN:VCARD\n' +
-    'VERSION:3.0\n' +
-    'FN:' + targetName + '\n' +
-    'ORG:undefined;\n' +
-    'TEL;type=CELL;type=VOICE;waid=' + targetNumber + ':+' + targetNumber + '\n' +
-    'END:VCARD';
-
-  zk.sendMessage(dest, {
-    contacts: {
-      displayName: targetName,
-      contacts: [{ vcard }],
-    },
-  }, { quoted: ms });
-});*/
-
 //const { keith } = require('../keizzah/keith');
+
 keith({
   nomCom: "vcard",
   desc: "to generate owner vcard number",
   categorie: "owner",
   reaction: "⚔️"
 }, async (dest, zk, commandeOptions) => {
-  const { ms, auteurMsgRepondu, arg, repondre, auteurMessage, infosMessage } = commandeOptions;
+  const { ms, auteurMsgRepondu, arg, nomAuteurMessage, repondre } = commandeOptions;
+  
+  let targetName;
+  let targetNumber;
 
-  if (!auteurMsgRepondu && !arg) {
-    return repondre('❌ Please mention a user or provide a phone number');
-  }
-
-  let targetNumber, targetName;
-
+  // Check if the command is a reply to a message
   if (auteurMsgRepondu) {
-    // Extract mentioned user's number and name
     targetNumber = auteurMsgRepondu.split('@')[0];
+    targetName = nomAuteurMessage || "User";
+  } 
+  // Check if arguments are provided
+  else if (arg && arg.length > 0) {
+    targetName = arg.join(" ").replace(/[0-9]/g, '').trim() || "User";
+    const possibleNumber = arg.join("").replace(/[^0-9]/g, '');
     
-    // Get the mentioned user's name from the message context
-    const mentionedUser = infosMessage.mentionedJidList?.find(jid => jid.includes(targetNumber));
-    if (mentionedUser) {
-      // Try to get the pushname (display name) of the mentioned user
-      const contact = await zk.fetchStatus(mentionedUser).catch(() => null);
-      targetName = contact?.status || "User";
-    } else {
-      targetName = "User";
-    }
-  } else {
-    // Handle case when arguments are provided instead of mention
-    const possibleNumber = arg[0].replace(/[^0-9]/g, '');
     if (possibleNumber.length > 5) {
       targetNumber = possibleNumber;
-      targetName = arg.length > 1 ? arg.slice(1).join(' ') : "User";
     } else {
-      return repondre('❌ Please provide a valid phone number');
+      return repondre('❌ Please provide a valid phone number or mention a user');
     }
+  } 
+  // No valid input provided
+  else {
+    return repondre('❌ Please mention a user or provide a phone number and name');
   }
 
-  const vcard =
+  // Validate we have required data
+  if (!targetNumber) {
+    return repondre('❌ Could not determine phone number');
+  }
+
+  // Create vcard
+  const vcard = 
     'BEGIN:VCARD\n' +
     'VERSION:3.0\n' +
     'FN:' + targetName + '\n' +
@@ -129,10 +87,16 @@ keith({
     'TEL;type=CELL;type=VOICE;waid=' + targetNumber + ':+' + targetNumber + '\n' +
     'END:VCARD';
 
-  await zk.sendMessage(dest, {
-    contacts: {
-      displayName: targetName,
-      contacts: [{ vcard }],
-    },
-  }, { quoted: ms });
+  // Send the contact
+  try {
+    await zk.sendMessage(dest, {
+      contacts: {
+        displayName: targetName,
+        contacts: [{ vcard }],
+      },
+    }, { quoted: ms });
+  } catch (error) {
+    console.error('Error sending vcard:', error);
+    repondre('❌ An error occurred while sending the contact');
+  }
 });
