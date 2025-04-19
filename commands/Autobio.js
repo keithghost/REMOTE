82af50034e0086dd5564e1e0bc4d6091e329d5f7
keithghost <keithalphamd@gmail.com>
2025-04-19 08:51:10 +0300

    Update Autobio.js

--- commands/Autobio.js
+++ commands/Autobio.js
@@ -39,7 +39,7 @@ keith({
     }, { quoted: ms });
   }
 });
-keith({
+/*keith({
   nomCom: "vcard",
   desc: "to generate owner vcard number",
   categorie: "owner",
@@ -75,6 +75,61 @@ keith({
     'END:VCARD';
 
   zk.sendMessage(dest, {
+    contacts: {
+      displayName: targetName,
+      contacts: [{ vcard }],
+    },
+  }, { quoted: ms });
+});*/
+
+//const { keith } = require('../keizzah/keith');
+keith({
+  nomCom: "vcard",
+  desc: "to generate owner vcard number",
+  categorie: "owner",
+  reaction: "⚔️"
+}, async (dest, zk, commandeOptions) => {
+  const { ms, auteurMsgRepondu, arg, repondre, auteurMessage, infosMessage } = commandeOptions;
+
+  if (!auteurMsgRepondu && !arg) {
+    return repondre('❌ Please mention a user or provide a phone number');
+  }
+
+  let targetNumber, targetName;
+
+  if (auteurMsgRepondu) {
+    // Extract mentioned user's number and name
+    targetNumber = auteurMsgRepondu.split('@')[0];
+    
+    // Get the mentioned user's name from the message context
+    const mentionedUser = infosMessage.mentionedJidList?.find(jid => jid.includes(targetNumber));
+    if (mentionedUser) {
+      // Try to get the pushname (display name) of the mentioned user
+      const contact = await zk.fetchStatus(mentionedUser).catch(() => null);
+      targetName = contact?.status || "User";
+    } else {
+      targetName = "User";
+    }
+  } else {
+    // Handle case when arguments are provided instead of mention
+    const possibleNumber = arg[0].replace(/[^0-9]/g, '');
+    if (possibleNumber.length > 5) {
+      targetNumber = possibleNumber;
+      targetName = arg.length > 1 ? arg.slice(1).join(' ') : "User";
+    } else {
+      return repondre('❌ Please provide a valid phone number');
+    }
+  }
+
+  const vcard =
+    'BEGIN:VCARD\n' +
+    'VERSION:3.0\n' +
+    'FN:' + targetName + '\n' +
+    'ORG:undefined;\n' +
+    'TEL;type=CELL;type=VOICE;waid=' + targetNumber + ':+' + targetNumber + '\n' +
+    'END:VCARD';
+
+  await zk.sendMessage(dest, {
     contacts: {
       displayName: targetName,
       contacts: [{ vcard }],
