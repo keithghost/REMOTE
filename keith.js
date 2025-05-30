b7d8f3cf35edc6c36987e8552ba28133a63c2c91
keithghost <keithalphamd@gmail.com>
2025-05-30 13:03:12 +0300

    Update keith.js

--- keith.js
+++ keith.js
@@ -92,7 +92,7 @@ async function authentification() {
 }
 authentification();
 
-const store = (0, baileys_1.makeInMemoryStore)({
+const store = (0, baileys_1.addTransactionCapability)({
     logger: pino().child({ level: "silent", stream: "store" }),
 });
 setTimeout(() => {
