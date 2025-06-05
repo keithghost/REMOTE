05535f9d5de5a5cc271d2049d0d8512720687955
keithghost <keithalphamd@gmail.com>
2025-06-05 16:51:58 +0300

    Update System.js

--- Cmds/System.js
+++ Cmds/System.js
@@ -1,1 +1,41 @@
+const { keith } = require('../commandHandler');
+const speed = require("performance-now");
+
+keith({
+    pattern: "ping",
+    alias: ["speed", "latency"],
+    desc: "To check bot speed",
+    category: "System",
+    react: "🖌️",
+    filename: __filename
+}, async (context) => {
+  const { client, m, botname, author } = context;
+
+  try {
+    // Correcting the timestamp calculation
+    const startTime = speed();
+    
+    let customContactMessage = {
+      key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
+      message: {
+        contactMessage: {
+          displayName: author,
+          vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;;\nFN:${author}\nitem1.TEL;waid=${m?.sender?.split('@')[0] ?? 'unknown'}:${m?.sender?.split('@')[0] ?? 'unknown'}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+        },
+      },
+    };
+
+    // Measure execution speed after creating the message object
+    const pingSpeed = speed() - startTime;
+
+    await client.sendMessage(
+      m.chat, 
+      { text: `${botname} speed\n\n *${pingSpeed.toFixed(4)} ms*` }, 
+      { quoted: customContactMessage }
+    );
+
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+});
 
