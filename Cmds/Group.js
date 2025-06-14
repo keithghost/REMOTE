b97bc167acf2e8f331ff0f4dfe73f27609691b20
keithghost <keithalphamd@gmail.com>
2025-06-14 18:56:21 +0300

    Update Group.js

--- Cmds/Group.js
+++ Cmds/Group.js
@@ -1,1 +1,22 @@
 
+const { keith } = require('../commandHandler');
+const middleware = require('../utility/botUtil/middleware');
+
+keith({
+    pattern: "open",
+    alias: ["groupopen", "opengroup"],
+    desc: "Chat with AI using Keith's API",
+    category: "Group",
+    react: "🗿",
+    filename: __filename
+}, async (context) => {
+    try {
+        await middleware(context, async () => {
+            const { client, m, reply } = context;
+            await client.groupSettingUpdate(m.chat, 'not_announcement');
+            reply('Group opened.');
+        });
+    } catch (error) {
+        console.error("Error updating group setting:", error);
+    }
+});
