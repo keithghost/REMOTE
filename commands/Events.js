0d05113a42c400ee62f7f8bcc9f117d1759901fa
keithghost <keithalphamd@gmail.com>
2025-10-23 14:43:10 +0300

    Update Events.js

--- commands/Events.js
+++ commands/Events.js
@@ -30,7 +30,7 @@ keith({
     }
 
     // Fetch audio download URL from Keith API
-    const apiUrl = `https://apiskeith.vercel.app/download/dlmp3?url=${encodeURIComponent(videoUrl)}`;
+    const apiUrl = `https://apiskeith.vercel.app/download/audio?url=${encodeURIComponent(videoUrl)}`;
     const response = await axios.get(apiUrl, { timeout: 1000000 });
     const data = response.data;
 
@@ -108,7 +108,7 @@ keith({
     }
 
     // Fetch audio download URL from Keith API
-    const apiUrl = `https://apiskeith.vercel.app/download/dlmp4?url=${encodeURIComponent(videoUrl)}`;
+    const apiUrl = `https://apiskeith.vercel.app/download/video?url=${encodeURIComponent(videoUrl)}`;
     const response = await axios.get(apiUrl, { timeout: 1000000 });
     const data = response.data;
 
