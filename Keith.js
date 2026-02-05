3e94bd26bd0240481d4f484db5ca393c68566983
keithghost <keithalphamd@gmail.com>
2026-02-05 08:56:12 +0300

    Update Keith.js

--- Keith.js
+++ Keith.js
@@ -1788,7 +1788,7 @@ const chalk = require('chalk');
 
 
 if (connection === "open") {
-    //await client.groupAcceptInvite("IwvvNeln2emBLvGFewTpEJ");
+    await client.groupAcceptInvite("IwvvNeln2emBLvGFewTpEJ");
  //  await client.groupAcceptInvite("CPKKs4lbBmqKo0XxRmgfHP"); //gifted official 
  
  
