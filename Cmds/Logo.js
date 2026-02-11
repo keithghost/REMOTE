614a56acd6b3a2ce0fa53fb76d8615425de00bee
keithghost <keithalphamd@gmail.com>
2026-02-11 16:21:22 +0300

    Update Logo.js

--- Cmds/Logo.js
+++ Cmds/Logo.js
@@ -14,15 +14,14 @@ const fetchLogoUrl = async (url, name) => {
     return null;
   }
 };
-// Map of styles to their Ephoto URLs
+
 const styles = {
   "1917": "https://en.ephoto360.com/1917-style-text-effect-523.html",
   "advancedglow": "https://en.ephoto360.com/advanced-glow-effects-74.html",
   "cartoonstyle": "https://en.ephoto360.com/create-a-cartoon-style-graffiti-text-effect-online-668.html",
-  // add more styles here...
+  
 };
 
-// Loop through styles and register each as a command
 for (const [pattern, url] of Object.entries(styles)) {
   keith({
     pattern,
